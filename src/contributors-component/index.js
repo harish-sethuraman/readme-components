1f789c486eb2724baadb6b6f79eb3fa9b43748a6
amareshsm <amareshsm13@gmail.com>
2020-08-24 00:50:27 +0530

    updated contributors component

--- src/contributors-component/index.js
+++ src/contributors-component/index.js
@@ -3,34 +3,30 @@ const imgFetcher = require("../utils/imgFetcher");
 const contributorsComponent = async (data = {}) => {
 return `<div class="container" xmlns="http://www.w3.org/1999/xhtml">
 <div class="header">
-      <div class="profile">
+      <div class="repo">
         <img
           class="image"
           src="https://static.thenounproject.com/png/368566-200.png"
           alt="Profile image"
           alm="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
         ></img>
-        <div class="userdetails ">
+        <div class="repo-details">
           <span class="name">
-            <a target="_blank" href="">
+            <a  href="">
                 readme-components Contributors
             </a>
           </span>
         </div>
       </div>
-      <div class="report">
-          
+      <div class="github-icon">
               <img
           class="image"
           src="https://image.flaticon.com/icons/png/512/25/25231.png"
           alt="Profile image"
- 
         ></img>
       </div>
     </div>
-
-
-      <div class="section1">
+      <div class="contributor">
       <div class="profile">
         <img
           class="image"
@@ -59,7 +55,7 @@ return `<div class="container" xmlns="http://www.w3.org/1999/xhtml">
       </div>
     </div>
  <div class="divider"></div>
- <div class="section1">
+ <div class="contributor">
       <div class="profile">
         <img
           class="image"
@@ -87,22 +83,6 @@ return `<div class="container" xmlns="http://www.w3.org/1999/xhtml">
         <span class="commits">20</span>
   
       </div>
-      </div>
-    </div>
-
-
-    <div class="section2" style="display: none;">
-      <div class="reputation">
-        <span class="text">REPUTATION</span>
-        <span class="value">20</span>
-      </div>
-      <div class="badges">
-        <span class="text ">BADGES</span>
-        <div class="badgeicon">
-          <span class="gold">1</span>
-          <span class="silver">2</span>
-          <span class="bronze">3</span>
-        </div>
       </div>
     </div>
       </div>`;
--- src/contributors-component/index.style.js
+++ src/contributors-component/index.style.js
@@ -23,134 +23,96 @@ const constributorsStyle = () => {
     background-color: #cccccc;
     padding:8px 17px;
     }
-    .header .profile{
+    .header .repo{
     display: flex;
     justify-content: space-around;
     align-items: center;
     }
-    .header .userdetails{
+    .header .repo-details{
     display: flex; flex-direction: column; margin-left: 12px;
     }
-    .header .userdetails .name {
+    .header .repo-details .name {
     font-weight: 500; color:#000; font-size: 12px;
     }
     .name:first-letter{
        text-transform: uppercase;
-    }
-    .header .userdetails .location{
-    font-weight: 400; color: #586069; font-size: 11px
-    }
+    } 
     .header .image{
     width: 18px; height: 18px; border-radius: 15%;
     }
-    .header .report{
+    .header .github-icon{
     display: flex;
     font-size:10px;
     }
-    .header .report .answers{
-    display: flex;
-    flex-direction: column;
-    margin:0 5px;
-    align-items: center;
-    font-weight: 600;
-    }
-    .header .report .questions{
-    display: flex;
-    flex-direction: column;
-    margin:0 5px;
-    align-items: center;
-    font-weight: 600;
-    }
-    .header .report .reached {
-    display: flex;
-    flex-direction: column;
-    margin:0 5px;
-    align-items: center;
-    font-weight: 600;
-    }
-    .header .report .answers::after{
-    content: "Answers"
-    }
-    .header .report .questions::after{
-    content: "Questions"
-    }
-    .header .report .reached::after{
-    content: "Profile views";
-    white-space: nowrap;
-    }
 
-
-
-
-
-    .section1{
+    .contributor{
     display: flex; align-items: center;
     justify-content: space-between;
     padding: 5px 17px  5px 17px;
     }
-    .section1 .profile{
+    .contributor .profile{
     display: flex;
     justify-content: space-around;
     align-items: center;
     }
-    .section1 .userdetails{
+    .contributor .userdetails{
     display: flex; flex-direction: column; margin-left: 12px
     }
-    .section1 .userdetails .name {
+    .contributor .userdetails .name {
     font-weight: 500; color:#ef8236; font-size: 12px;
     }
     .name:first-letter{
        text-transform: uppercase;
     }
-    .section1 .userdetails .location{
+    .contributor .userdetails .location{
     font-weight: 400; color: #586069; font-size: 11px
     }
-    .section1 .image{
+    .contributor .image{
     width: 25px; height: 25px; border-radius: 50%;
     }
-    .section1 .report{
+    .contributor .report{
     display: flex;
     font-size:10px;
     }
-    .section1 .report .answers{
+    .contributor .report .answers{
     display: flex;
     flex-direction: column;
     margin:0 5px;
     align-items: center;
     font-weight: 600;
     }
-    .section1 .report .questions{
+    .contributor .report .questions{
     display: flex;
     flex-direction: column;
     margin:0 5px;
     align-items: center;
     font-weight: 600;
     }
-    .section1 .report .reached {
+    .contributor .report .reached {
     display: flex;
     flex-direction: column;
     margin:0 5px;
     align-items: center;
     font-weight: 600;
     }
-    .section1 .report .answers::after{
+    .contributor .report .answers::after{
     content: "Answers"
     }
-    .section1 .report .questions::after{
+    .contributor .report .questions::after{
     content: "Questions"
     }
-    .section1 .report .reached::after{
+    .contributor .report .reached::after{
     content: "Profile views";
     white-space: nowrap;
     }
-    .section1 .report .legend{
+    .contributor .report .legend{
     	list-style-type: none;
     	margin: 0px;
     	display: flex;
     	justify-content: center;
     	align-items: center;
     }
-    .section1 .report .legend li{
+    .contributor .report .legend li{
     	    display: inline-block;
     width: 10px;
     height: 10px;
@@ -158,63 +120,7 @@ const constributorsStyle = () => {
     border-radius: 2px;
     box-shadow: inset 0 0 0 1px rgba(27,31,35,.04);
     }
-    .section2{
-    margin-top: 12px; display: flex; justify-content: space-between; align-items: center;
-    }
-    .section2 .reputation{
-    display: flex; flex-direction: column;
-    }
-    .section2 .reputation .text{
-    font-size: 10px; font-weight: 500; color: #586069;
-    }
-    .section2 .reputation .value{
-    font-weight: 600; color:#211F1F; font-size: 30px; line-height: 1;
-    }
-    .section2 .badges{
-    display: flex; flex-direction: column; margin-top: -4px;
-    }
-    .section2 .badges .text{
-    font-size: 11px; font-weight: 500; color: #586069;
-    }
-    .section2 .badges .badgeicon{
-    display: flex; flex-direction: row; font-weight: 400; color: #211F1F; font-size: 10px; margin-top: 2px;
-    }
-    .section2 .badges .badgeicon .gold {
-    display: flex;
-    min-width: 45px; text-align: center; padding: 4px 4px 4px 0px; background-color: #fff4d1; border: 1px solid #f1b600; border-radius: 5px
-    }
-    .section2 .badges .badgeicon .silver::before{
-    content: '';
-    margin: auto 10px;
-    border-radius: 50%;
-    background-color: #9e9e9e;
-    height: 7.5px;
-    width: 7.5px;
-    }
-    .section2 .badges .badgeicon .silver{
-    display: flex;
-    min-width: 45px; text-align: center; padding: 4px 4px 4px 0px; background-color: #e8e8e8; margin: 0 4px 0 4px; border: 1px solid #9a9c9f; border-radius: 5px;
-    }
-    .section2 .badges .badgeicon .gold::before{
-    content: '';
-    margin: auto 10px;
-    border-radius: 50%;
-    background-color: #FFC107;
-    height: 7.5px;
-    width: 7.5px;
-    }
-    .section2 .badges .badgeicon .bronze{
-    display: flex;
-    min-width: 45px; text-align: center; padding: 4px 4px 4px 0px; background-color: #f2e9e1; border: 1px solid #ab825f; border-radius: 5px;
-    }
-    .section2 .badges .badgeicon .bronze::before{
-    content: '';
-    margin: auto 10px;
-    border-radius: 50%;
-    background-color: #c68d79;
-    height: 7.5px;
-    width: 7.5px;
-    }
+
     .dark-card{
         color:#f6f6f6;
         background-color:#2a2a2a;
